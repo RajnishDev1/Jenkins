be1a52b74fadaae299c478fdf3e50fb8b2e81853
RajnishDev1 <Rajnishkumar022@gmail.com>
2026-05-02 11:31:24 +0000

    Create Jenkins.java

---
+++ Jenkins.java
@@ -0,0 +1,6 @@
+public class Hello{
+public static void main(String[] args) {
+//prints "Jenkin data is get pulled from GITHUB"
+system.out.println("-------Jenkin data is get pulled from GITHUB---------");
+}
+}

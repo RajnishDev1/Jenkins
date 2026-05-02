41a4d801b7a31acd784acf4e02f0fa18c14f3bb1
RajnishDev1 <Rajnishkumar022@gmail.com>
2026-05-02 12:16:34 +0000

    Fix print statement formatting in Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,6 +1,6 @@
 public class Jenkins{
 public static void main(String[] args) {
 //prints "Jenkin data is get pulled from GITHUB"
-system.out.println("-------Jenkin data is get pulled from GITHUB---------");
+System.out.println("-------Jenkin data is get pulled from GITHUB---------");
 }
 }
